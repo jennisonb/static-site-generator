83cf188fc482c87fe81b6e8e48e675fc85c6c77c
jennisonb <jennisonb@gmail.com>
2025-04-24 20:05:31 -0600

    latest from msi laptop

--- src/main.py
+++ src/main.py
@@ -3,5 +3,4 @@ import inspect
 from htmlnode import HTMLNode
 
 if __name__ == "__main__":
-    example = HTMLNode("", "", "", {"href": "https://www.google.com", "target": "_blank",})
-    print(example.props_to_html())
+    print("in main function...")
